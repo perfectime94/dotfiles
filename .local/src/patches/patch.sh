07700f30446739bfb004138901ab0d8093838616
Lineu Martins <lineumartins001@gmail.com>
2021-06-30 13:01:33 +0200

    patches.sh: improve output

--- .local/src/patches/patch.sh
+++ .local/src/patches/patch.sh
@@ -3,10 +3,9 @@
 project=$1
 cd ~/.local/src/$project
 for m in $(ls ~/.local/src/patches/$project); do
-	echo $m
 	if [[ $m == *"#"* ]]
 	then
-		echo -e "\n" "ommited custom patch" "\n"
+		echo -e "\n" "ommited custom patch" "$m" "\n"
 		continue
 	else
 		echo -e "\n" "patching" $m "\n"
